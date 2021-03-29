95a512fe524cd98e0b57b7819342eb74b2150e78
xiamu <myjyixi@163.com>
2021-03-29 16:36:47 +0800

    feat: node做主项目

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: 'view',
+  publicDir: 'public',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true
+  },
   plugins: [vue()]
 })
