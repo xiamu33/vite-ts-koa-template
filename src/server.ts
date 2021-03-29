62209687d4986eb5fa0ccda2e72fb5e81ee2d17c
xiamu <myjyixi@163.com>
2021-03-29 19:36:49 +0800

    feat: add koa-static

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
 import Koa from 'koa'
+import KoaStatic from 'koa-static';
+import path from 'path'
 
 const app = new Koa();
+
+app.use(KoaStatic(path.join(__dirname, '../dist')))
 
 app.use(ctx => {
   ctx.body = 'hello world'
