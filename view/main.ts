84639be318a79e8ebdfb250db1f2afb22b2517dd
xiamu <myjyixi@163.com>
2021-04-22 21:23:08 +0800

    feat: add sass

--- view/main.ts
+++ view/main.ts
@@ -1,6 +1,7 @@
 import App from './App.vue';
 import { createApp } from 'vue';
 import { emitCreateApp } from './utils';
+import './styles/index.scss';
 import './plugins';
 
 const app = createApp(App);
