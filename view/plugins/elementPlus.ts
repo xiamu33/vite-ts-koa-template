f3797e68a3fdb4317e32fc5b52e3e9da9902b7cf
xiamu <myjyixi@163.com>
2021-11-16 17:46:48 +0800

    🚑 Quickfix(element): 升级element-plus后修正css导入路径

--- view/plugins/elementPlus.ts
+++ view/plugins/elementPlus.ts
@@ -1,7 +1,7 @@
 import ElementPlus from 'element-plus';
 import { onCreateApp } from '../utils';
-import 'element-plus/lib/theme-chalk/index.css';
+import 'element-plus/theme-chalk/index.css';
 
 onCreateApp(app => {
-  app?.use(ElementPlus);
+  app!.use(ElementPlus);
 });
